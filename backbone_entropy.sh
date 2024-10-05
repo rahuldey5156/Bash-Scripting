67986a6cca335822b8df7b20197b253f68e6d050
Rahul Dey <rahuldey5156@gmail.com>
2024-10-05 13:18:26 +0530

    Delete backbone_entropy.sh

--- backbone_entropy.sh
+++
@@ -1,7 +0,0 @@
-echo 4 | gmx trjconv -f chainA.xtc -s chainA.pdb -b 295000 -o chainA_backbone_last_5ns.xtc
-
-echo 4 | gmx trjconv -f chainA.xtc -s chainA.pdb -dump 0 -o chainA_backbone_0ns.pdb
-
-echo 0 0 | gmx covar -f chainA_backbone_last_5ns.xtc -s chainA_backbone_0ns.pdb -o eig_backbone.xvg -v eig_backbone.trr -ascii eig_backbone.dat
-
-gmx anaeig -f chainA_backbone_last_5ns.xtc -v eig_backbone.trr -entropy -temp 310 -s chainA_backbone_0ns.pdb -comp eig_backbone.xvg
